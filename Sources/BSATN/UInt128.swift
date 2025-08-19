2248aa159e2d0c1db8296767a3f9128ad9613fb0
Dave Poirier <dave@encoded.life>
2025-08-19 04:32:10 -0400

    CustomStringConvertible

--- Sources/BSATN/UInt128.swift
+++ Sources/BSATN/UInt128.swift
@@ -1,4 +1,10 @@
-public struct UInt128: Equatable, Hashable {
+public struct UInt128: Equatable, Hashable, CustomStringConvertible {
     public let u0: UInt64
     public let u1: UInt64
+    
+    public var description: String {
+        let hex0 = String(format: "%016llx", u0)
+        let hex1 = String(format: "%016llx", u1)
+        return hex1 + hex0
+    }
 }
--- Sources/BSATN/UInt256.swift
+++ Sources/BSATN/UInt256.swift
@@ -1,6 +1,14 @@
-public struct UInt256: Equatable, Hashable {
+public struct UInt256: Equatable, Hashable, CustomStringConvertible {
     public let u0: UInt64
     public let u1: UInt64
     public let u2: UInt64
     public let u3: UInt64
+    
+    public var description: String {
+        let hex0 = String(format: "%016llx", u0)
+        let hex1 = String(format: "%016llx", u1)
+        let hex2 = String(format: "%016llx", u2)
+        let hex3 = String(format: "%016llx", u3)
+        return hex3 + hex2 + hex1 + hex0
+    }
 }
